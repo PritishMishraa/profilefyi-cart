f7a0f1a4c91a60ed921cee6c83ccc2d67677e5ce
Pritish <pritishmishra579@gmail.com>
2024-08-14 20:49:32 +0530

    opti

--- src/components/client/CartItems.tsx
+++ src/components/client/CartItems.tsx
@@ -3,6 +3,7 @@
 import { api } from "@/trpc/react";
 import { type Cart } from "@prisma/client";
 import Image from "next/image";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function CartItems({ initalCart }: { initalCart: Cart[] }) {
@@ -26,36 +27,89 @@ export default function CartItems({ initalCart }: { initalCart: Cart[] }) {
     });
 
     const updateQuantity = api.post.updateQuantity.useMutation({
+        onMutate: async (newQunatity) => {
+            await utils.post.getCart.cancel();
+
+            const prevData = utils.post.getCart.getData();
+
+            utils.post.getCart.setData(undefined, (old) => {
+                if (!old) return;
+
+                const newData = old.map((product) => {
+                    if (product.productId === newQunatity.productId) {
+                        return {
+                            ...product,
+                            quantity: newQunatity.quantity,
+                        };
+                    }
+
+                    return product;
+                });
+
+                return newData;
+            });
+
+            return prevData;
+        },
         onSuccess: async () => {
+            await utils.post.getCart.invalidate();
+        },
+        onError: async (_err, _newPost, ctx) => {
+            utils.post.getCart.setData(undefined, ctx);
+        },
+        onSettled: async () => {
             await utils.post.getCart.invalidate();
         }
     });
 
+    const [discount, setDiscount] = useState<number | null>(null);
+
+    const calculateSubtotal = () => {
+        return products.data.reduce((total, product) => total + product.price * product.quantity, 0);
+    };
+
+    const calculateTotal = () => {
+        const subtotal = calculateSubtotal();
+        if (!discount) return subtotal;
+
+        const isPercentageDiscount = discount < 1;
+        return isPercentageDiscount ? subtotal * (1 - discount) : subtotal - discount;
+    };
+
+    const applyDiscount = (type: "fixed" | "percentage", value: number) => {
+        setDiscount(type === "percentage" ? value / 100 : value);
+    };
+
     return (
         <div className="h-full flex flex-col p-8 w-full">
-            <h2 className="mb-4 text-2xl font-bold leading-10 text-gray-800">
-                Your Cart
-            </h2>
             {products.data.length === 0 && (
                 <p className="mb-4 grow text-lg leading-7 text-gray-600">
                     You have 0 items in your cart.
                 </p>
             )}
             {products.data.map((product) => (
-                <div key={product.productId} className="flex flex-col md:flex-row grow items-start justify-between border-b py-4">
-                    <div className="flex items-center">
+                <div key={product.productId} className="flex flex-col md:flex-row grow items-center justify-between border-b py-4 gap-2 md:gap-0">
+                    <div className="flex items-center w-full">
                         <Image src={product.image} alt={product.name} width={80} height={80} className="mr-4" />
                         <div>
                             <h3 className="font-bold text-black">{product.name}</h3>
-                            <p className="text-gray-600">
-                                {product.price.toLocaleString("en-IN", {
-                                    style: "currency",
-                                    currency: "INR",
-                                })}
-                            </p>
+                            <div className="flex items-center gap-2">
+                                <p className="text-gray-600 text-xs">
+                                    {product.price.toLocaleString("en-IN", {
+                                        style: "currency",
+                                        currency: "INR",
+                                    })} x {product.quantity}
+                                </p> :
+                                <p className="text-gray-600 text-xs font-bold">
+                                    {(product.price * product.quantity).toLocaleString("en-IN", {
+                                        style: "currency",
+                                        currency: "INR",
+                                    })}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex items-center">
+                    <div className="flex items-center justify-end w-full">
                         <button
                             onClick={() => toast.promise(updateQuantity.mutateAsync({ productId: product.productId, quantity: product.quantity - 1 }), {
                                 loading: "Reducing quantity",
@@ -91,21 +145,66 @@ export default function CartItems({ initalCart }: { initalCart: Cart[] }) {
                     </div>
                 </div>
             ))}
+            {products.data.length > 0 && (
+                <>
+                    <div className="mt-8 p-4 bg-gray-100 rounded-lg">
+                        <h3 className="text-xl font-semibold text-gray-800">Cart Summary</h3>
+                        <p className="text-lg text-gray-700">
+                            Subtotal:{" "}
+                            {calculateSubtotal().toLocaleString("en-IN", {
+                                style: "currency",
+                                currency: "INR",
+                            })}
+                        </p>
+                        <p className="text-lg text-gray-700">
+                            Discount:{" "}
+                            {discount
+                                ? (discount < 1 ? `${discount * 100}%` : discount.toLocaleString("en-IN", {
+                                    style: "currency",
+                                    currency: "INR",
+                                }))
+                                : "No discount applied"}
+                        </p>
+                        <p className="text-lg font-bold text-gray-800">
+                            Total:{" "}
+                            {calculateTotal().toLocaleString("en-IN", {
+                                style: "currency",
+                                currency: "INR",
+                            })}
+                        </p>
+                        <div className="mt-4 flex gap-4">
+                            <button
+                                className="px-4 py-2 bg-blue-800 text-white rounded-lg"
+                                onClick={() => applyDiscount("fixed", 500)}
+                            >
+                                Apply ₹500 Discount
+                            </button>
+                            <button
+                                className="px-4 py-2 bg-blue-800 text-white rounded-lg"
+                                onClick={() => applyDiscount("percentage", 10)}
+                            >
+                                Apply 10% Discount
+                            </button>
+                        </div>
+                    </div>
+                </>
+            )}
+
             <div className="flex justify-between w-full pb-8">
                 <button
-                    className="mt-6 px-4 py-2 text-lg font-bold text-white bg-green-800 rounded-lg"
+                    className="mt-6 px-4 py-2 text-lg font-bold text-white bg-red-500 rounded-lg"
                     onClick={() => {
                         toast.promise(clearCart.mutateAsync(), {
                             loading: "Clearing cart",
                             success: "Cart cleared",
                             error: "Failed to clear cart",
-                        });
+                        })
                     }
                     }
                 >
                     Clear Cart
                 </button>
-                <button className="mt-6 px-4 py-2 text-lg font-bold text-white bg-blue-800 rounded-lg">
+                <button className="mt-6 px-4 py-2 text-lg font-bold text-white bg-green-600 rounded-lg">
                     Checkout
                 </button>
             </div>
--- src/components/server/ProductCard.tsx
+++ src/components/server/ProductCard.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import AddToCart from "../client/AddToCart";
 
 const ProductCard = ({
@@ -18,38 +17,36 @@ const ProductCard = ({
   small?: boolean;
 }) => (
   <div className="p-2 flex flex-col">
-    <Link href={`/products/${id}`}>
-      <Image
-        className={`aspect-[2/2] rounded-md object-cover border-2 border-gray-800`}
-        src={image ?? ""}
-        alt={`${name} image`}
-        width={1024}
-        height={1024}
-      />
-      <div>
-        {name && (
-          <h3
-            className={`mt-2 font-bold leading-10 text-gray-800 ${small ? "" : "text-xl"
-              }`}
-          >
-            {name}
-          </h3>
-        )}
-        {!small && price && (
-          <div className="my-1 text-md leading-5 text-gray-600">
-            {price.toLocaleString("en-IN", {
-              style: "currency",
-              currency: "INR",
-            })}
-          </div>
-        )}
-        {!small && description && (
-          <div className="mt-1 text-sm leading-5 text-gray-600 font-light italic">
-            {description}
-          </div>
-        )}
-      </div>
-    </Link>
+    <Image
+      className={`aspect-[2/2] rounded-md object-cover border-2 border-gray-800`}
+      src={image ?? ""}
+      alt={`${name} image`}
+      width={1024}
+      height={1024}
+    />
+    <div>
+      {name && (
+        <h3
+          className={`mt-2 font-bold leading-10 text-gray-800 ${small ? "" : "text-xl"
+            }`}
+        >
+          {name}
+        </h3>
+      )}
+      {!small && price && (
+        <div className="my-1 text-md leading-5 text-gray-600">
+          {price.toLocaleString("en-IN", {
+            style: "currency",
+            currency: "INR",
+          })}
+        </div>
+      )}
+      {!small && description && (
+        <div className="mt-1 text-sm leading-5 text-gray-600 font-light italic h-10">
+          {description}
+        </div>
+      )}
+    </div>
     <AddToCart productId={id} />
   </div>
 );
